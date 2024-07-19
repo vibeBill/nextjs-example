5930e8c7c985145416247e6eae9059d9454c0ef7
vibeBill <bill.zhu@vibe.us>
2024-07-19 17:39:16 +0800

    Final commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Customers() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+export const metadata: Metadata = { title: "Customers" };
+
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
